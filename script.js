390dac626730bc2ab365824e8fc9414be78b59f3
logout <logout>
2026-09-04 17:41:17 +0800

    addEventListener

--- script.js
+++ script.js
@@ -22,17 +22,38 @@ function greet(greetings, names) {
 console.log(greet("Mayung aga", ["Mark", "Bai", "Soy", "Tol", "Erp"]));
 
 const heading = document.querySelector("h1");
-heading.textContent = "Your father's Name";
+heading.textContent = "Reginald Maling";
 heading.style.color = "purple";
 heading.style.backgroundColor = "yellow";
 heading.style.fontSize = "100px";
 console.log(heading);
+
+heading.addEventListener("click", function() {
+    heading.style.color = "red";
+    console.log("Heading clicked!");
+});
 
 const contactheading = document.querySelector('#contact h2');
 console.log(contactheading);
 
 const serviceheading = document.querySelector('#services h2');
 console.log(serviceheading);
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener('click', function() {
+     isOff =!isOff;
+
+        if (isOff) {
+        body.style.backgroundColor ='black';
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+     }
+})
 
 
 
