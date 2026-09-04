14023402778c8bd4af830b6365e9a1b279336f5e
logout <logout>
2026-09-04 15:45:11 +0800

    fucntions

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let age = 30;
 age = 22;
 number = 123456789;
 address = "Naglo";
+
+console.log('Name: ${myName}');
+console.log('Age:  ${age}');
+console.log('Number: ${number}');
+console.log('Address: ${address}');
+console.log(67);
+
+function greet(name) {
+    if (Array.isArray(names)) {
+        name = name.joins(', ');
+    }
+    return '${greetings} sa enyu ${names}';
+}
+console.log(greet("Mayung aga", ["Mark", "Bai", "Soy", "Tol", "Erp"]));
+
+
+
